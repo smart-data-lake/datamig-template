26b18468dcc736774a771b1fdcb718eb36092ca4
Nikolaus Thiel <klt@fsfe.org>
2026-04-16 15:26:01 +0200

    connections driver as environment variable

--- config/connection.zsh
+++ config/connection.zsh
@@ -8,12 +8,14 @@
 case ${ENVIRONMENT} in
 loc)
     export SOURCE_DB="sdlb"
+    export SOURCE_DRIVER="org.postgresql.Driver"
     export SOURCE_HOST="localhost"
     export SOURCE_PORT="5432"
     export SOURCE_PW="sdlb"
     export SOURCE_USER="sdlb"
 
     export TARGET_DB="sdlb"
+    export TARGET_DRIVER="org.postgresql.Driver"
     export TARGET_HOST="localhost"
     export TARGET_PORT="5432"
     export TARGET_PW="sdlb"
@@ -21,11 +23,13 @@ loc)
     ;;
 *)
     export SOURCE_DB=""
+    export SOURCE_DRIVER=""
     export SOURCE_HOST=""
     export SOURCE_PORT=""
     export SOURCE_USER=""
 
     export TARGET_DB=""
+    export TARGET_DRIVER=""
     export TARGET_HOST=""
     export TARGET_PORT=""
     export TARGET_USER=""
